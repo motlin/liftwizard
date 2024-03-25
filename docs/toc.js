fccfe267ebdc0e9abb655a77a5f7f4b0a8f6957f
motlin <motlin@users.noreply.github.com>
2024-03-25 02:04:20 +0000

    [skip ci] deploy to gh-pages 1f33e32886028294a4a9714e282757abc4707f37

---
+++ docs/toc.js
@@ -0,0 +1,273 @@
+toc = [ {
+  "chapterTitle" : "",
+  "dirName" : "",
+  "items" : [ {
+    "chapterTitle" : "",
+    "pageTitle" : "",
+    "pageMeta" : { },
+    "dirName" : "",
+    "fileName" : "index",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Liftwizard",
+      "id" : "liftwizard"
+    } ]
+  } ]
+}, {
+  "chapterTitle" : "Configuration",
+  "dirName" : "configuration",
+  "items" : [ {
+    "chapterTitle" : "Configuration",
+    "pageTitle" : "Environment Variables",
+    "pageMeta" : { },
+    "dirName" : "configuration",
+    "fileName" : "environment-variables",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Configuration",
+    "pageTitle" : "Json5 Configuration",
+    "pageMeta" : { },
+    "dirName" : "configuration",
+    "fileName" : "json5-configuration",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Configuration through json5 instead of yaml",
+      "id" : "configuration-through-json5-instead-of-yaml"
+    } ]
+  }, {
+    "chapterTitle" : "Configuration",
+    "pageTitle" : "ConfigLoggingBundle",
+    "pageMeta" : { },
+    "dirName" : "configuration",
+    "fileName" : "ConfigLoggingBundle",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Jackson",
+  "dirName" : "jackson",
+  "items" : [ {
+    "chapterTitle" : "Jackson",
+    "pageTitle" : "ObjectMapperBundle",
+    "pageMeta" : { },
+    "dirName" : "jackson",
+    "fileName" : "ObjectMapperBundle",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Logging",
+  "dirName" : "logging",
+  "items" : [ {
+    "chapterTitle" : "Logging",
+    "pageTitle" : "JerseyHttpLoggingBundle",
+    "pageMeta" : { },
+    "dirName" : "logging",
+    "fileName" : "JerseyHttpLoggingBundle",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Logging",
+    "pageTitle" : "Buffered Logging",
+    "pageMeta" : { },
+    "dirName" : "logging",
+    "fileName" : "buffered-logging",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Logging",
+    "pageTitle" : "Filter Factories",
+    "pageMeta" : { },
+    "dirName" : "logging",
+    "fileName" : "filter-factories",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Graphql",
+  "dirName" : "graphql",
+  "items" : [ {
+    "chapterTitle" : "Graphql",
+    "pageTitle" : "Bundle",
+    "pageMeta" : { },
+    "dirName" : "graphql",
+    "fileName" : "bundle",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Graphql",
+    "pageTitle" : "Instrumentation Logging",
+    "pageMeta" : { },
+    "dirName" : "graphql",
+    "fileName" : "instrumentation-logging",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Graphql",
+    "pageTitle" : "Instrumentation Metrics",
+    "pageMeta" : { },
+    "dirName" : "graphql",
+    "fileName" : "instrumentation-metrics",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Graphql",
+    "pageTitle" : "Data Fetcher Async",
+    "pageMeta" : { },
+    "dirName" : "graphql",
+    "fileName" : "data-fetcher-async",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Reladomo",
+  "dirName" : "reladomo",
+  "items" : [ {
+    "chapterTitle" : "Reladomo",
+    "pageTitle" : "Reladomo Operation Compiler",
+    "pageMeta" : { },
+    "dirName" : "reladomo",
+    "fileName" : "reladomo-operation-compiler",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Database",
+  "dirName" : "database",
+  "items" : [ {
+    "chapterTitle" : "Database",
+    "pageTitle" : "Named Data Source",
+    "pageMeta" : { },
+    "dirName" : "database",
+    "fileName" : "named-data-source",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Database",
+    "pageTitle" : "Liquibase Migrations",
+    "pageMeta" : { },
+    "dirName" : "database",
+    "fileName" : "liquibase-migrations",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+}, {
+  "chapterTitle" : "Temporal Data",
+  "dirName" : "temporal-data",
+  "items" : [ {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Temporal Data Overview",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "temporal-data-overview",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Temporal features",
+      "id" : "temporal-features"
+    } ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Running Example",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "running-example",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Non Destructive Updates",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "non-destructive-updates",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "POST Request Body",
+      "id" : "post-request-body"
+    }, {
+      "title" : "POST Response Body",
+      "id" : "post-response-body"
+    }, {
+      "title" : "Temporal Response",
+      "id" : "temporal-response"
+    }, {
+      "title" : "Non-destructive updates",
+      "id" : "non-destructive-updates"
+    }, {
+      "title" : "Response",
+      "id" : "response"
+    }, {
+      "title" : "As-of query",
+      "id" : "as-of-query"
+    } ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "As Of Queries",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "as-of-queries",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Temporal Schema",
+      "id" : "temporal-schema"
+    }, {
+      "title" : "Temporal queries in SQL",
+      "id" : "temporal-queries-in-sql"
+    } ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Versioning",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "versioning",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ {
+      "title" : "Query as-of version",
+      "id" : "query-as-of-version"
+    }, {
+      "title" : "Version queries in SQL",
+      "id" : "version-queries-in-sql"
+    }, {
+      "title" : "Composites",
+      "id" : "composites"
+    }, {
+      "title" : "Response",
+      "id" : "response"
+    }, {
+      "title" : "Ownership direction",
+      "id" : "ownership-direction"
+    } ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Auditing",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "auditing",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Optimistic Locking",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "optimistic-locking",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Diffs",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "diffs",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  }, {
+    "chapterTitle" : "Temporal Data",
+    "pageTitle" : "Maker Checker Workflows",
+    "pageMeta" : { },
+    "dirName" : "temporal-data",
+    "fileName" : "maker-checker-workflows",
+    "viewOnRelativePath" : null,
+    "pageSectionIdTitles" : [ ]
+  } ]
+} ]
